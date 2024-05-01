51ba5db2d2ea4c6f0a4cfd54e790c15810dba753
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-01 19:41:13 +0530

    add ContainersConfig for integration tests

--- inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/InventoryServiceApplicationTests.java
@@ -2,8 +2,10 @@ package io.hrushik09.ecommerce.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(ContainersConfig.class)
 class InventoryServiceApplicationTests {
 
     @Test
--- inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/TestInventoryServiceApplication.java
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/TestInventoryServiceApplication.java
@@ -2,22 +2,12 @@ package io.hrushik09.ecommerce.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestInventoryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(InventoryServiceApplication::main).with(TestInventoryServiceApplication.class).run(args);
-    }
-
-    @Bean
-    @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+        SpringApplication.from(InventoryServiceApplication::main).with(ContainersConfig.class).run(args);
     }
 
 }
