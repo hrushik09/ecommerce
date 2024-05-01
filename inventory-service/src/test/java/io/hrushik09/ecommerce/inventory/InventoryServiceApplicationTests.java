94c9c12279428a0fd668cb11a7710b1cd130c36f
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-01 19:28:03 +0530

    add inventory-service module

---
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hrushik09.ecommerce.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
