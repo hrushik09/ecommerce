32cfede5069d931074ac2489937695e4f6fad079
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-02 22:36:26 +0530

    add AbstractIT

--- inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/InventoryServiceApplicationTests.java
@@ -1,15 +1,9 @@
 package io.hrushik09.ecommerce.inventory;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@SpringBootTest
-@Import(ContainersConfig.class)
-class InventoryServiceApplicationTests {
-
+class InventoryServiceApplicationTests extends AbstractIT {
     @Test
     void contextLoads() {
     }
-
 }
