8aeb1599cbffbfe7c57eaeee17869100b04044ee
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-03 00:17:37 +0530

    add end-to-end test for creating item

---
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/web/items/ItemEndToEndTest.java
@@ -0,0 +1,32 @@
+package io.hrushik09.ecommerce.inventory.web.items;
+
+import io.hrushik09.ecommerce.inventory.AbstractIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+class ItemEndToEndTest extends AbstractIT {
+    @Test
+    void shouldCreateItemSuccessfully() {
+        given().contentType(JSON)
+                .body("""
+                        {
+                        "name": "Item 103",
+                        "category": "Category 4",
+                        "quantity": 10
+                        }
+                        """)
+                .when()
+                .post("/api/items")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("$.code", notNullValue())
+                .body("$.name", equalTo("Item 103"))
+                .body("$.category", equalTo("Category 4"))
+                .body("$.quantity", equalTo(10));
+    }
+}
