7649a5b52a312bfd027093701d6bfe60803112d4
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-04 12:15:22 +0530

    rename package

--- api-gateway/src/main/java/io/hrushik09/ecommerce/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/io/hrushik09/ecommerce/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package io.hrushik09.apigateway;
+package io.hrushik09.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- api-gateway/src/test/java/io/hrushik09/ecommerce/gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/io/hrushik09/ecommerce/gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package io.hrushik09.apigateway;
+package io.hrushik09.ecommerce.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
