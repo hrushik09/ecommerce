754c5de814df58db07812f15cfc70dbbc158a0ed
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-04 18:20:38 +0530

    add missing Validated annotation

--- webapp/src/main/java/io/hrushik09/ecommerce/webapp/ApplicationProperties.java
+++ webapp/src/main/java/io/hrushik09/ecommerce/webapp/ApplicationProperties.java
@@ -1,7 +1,9 @@
 package io.hrushik09.ecommerce.webapp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "ecommerce")
 public record ApplicationProperties(String apiGatewayUrl) {
 }
