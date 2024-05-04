657645e5ad9c90489b37e5633c4702b647629569
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-04 11:00:29 +0530

    add end-to-end test for duplicate item

--- inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/web/items/ItemEndToEndTest.java
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/web/items/ItemEndToEndTest.java
@@ -1,6 +1,7 @@
 package io.hrushik09.ecommerce.inventory.web.items;
 
 import io.hrushik09.ecommerce.inventory.AbstractIT;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -10,23 +11,56 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 class ItemEndToEndTest extends AbstractIT {
-    @Test
-    void shouldCreateItemSuccessfully() {
-        given().contentType(JSON)
-                .body("""
+    @Nested
+    class CreateItem {
+        @Test
+        void shouldCreateItemSuccessfully() {
+            given().contentType(JSON)
+                    .body("""
                         {
                         "name": "Item 103",
                         "category": "Category 4",
                         "quantity": 10
                         }
                         """)
-                .when()
-                .post("/api/items")
-                .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .body("code", notNullValue())
-                .body("name", equalTo("Item 103"))
-                .body("category", equalTo("Category 4"))
-                .body("quantity", equalTo(10));
+                    .when()
+                    .post("/api/items")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("code", notNullValue())
+                    .body("name", equalTo("Item 103"))
+                    .body("category", equalTo("Category 4"))
+                    .body("quantity", equalTo(10));
+        }
+
+        @Test
+        void shouldNotCreateIfItemWithNameAndDescriptionAlreadyExists() {
+            given().contentType(JSON)
+                    .body("""
+                            {
+                            "name": "Item 1",
+                            "category": "Category 1",
+                            "quantity": 3
+                            }
+                            """)
+                    .when()
+                    .post("/api/items")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value());
+
+            given().contentType(JSON)
+                    .body("""
+                            {
+                            "name": "Item 1",
+                            "category": "Category 1",
+                            "quantity": 7
+                            }
+                            """)
+                    .when()
+                    .post("/api/items")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value())
+                    .body("detail", equalTo("Item Item 1 already exists in category Category 1"));
+        }
     }
 }
