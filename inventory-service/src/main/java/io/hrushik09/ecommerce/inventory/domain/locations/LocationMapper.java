fb533f4d3b10bb5a2d7df6bd5de4d21f60c40253
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-06 21:33:34 +0530

    remove extra method, rename

--- inventory-service/src/main/java/io/hrushik09/ecommerce/inventory/domain/locations/LocationMapper.java
+++ inventory-service/src/main/java/io/hrushik09/ecommerce/inventory/domain/locations/LocationMapper.java
@@ -1,14 +1,9 @@
 package io.hrushik09.ecommerce.inventory.domain.locations;
 
 import io.hrushik09.ecommerce.inventory.domain.locations.model.CreateLocationResponse;
-import io.hrushik09.ecommerce.inventory.domain.locations.model.LocationSummary;
 
 class LocationMapper {
     public static CreateLocationResponse convertToCreateLocationResponse(LocationEntity locationEntity) {
         return new CreateLocationResponse(locationEntity.getCode(), locationEntity.getName(), locationEntity.getAddress());
-    }
-
-    public static LocationSummary convertToLocationSummary(LocationEntity locationEntity) {
-        return new LocationSummary(locationEntity.getCode(), locationEntity.getName(), locationEntity.getAddress());
     }
 }
