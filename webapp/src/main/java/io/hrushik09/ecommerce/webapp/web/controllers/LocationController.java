dd02e8e64b17e6c969879bf0287e38c540f34c03
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-07 09:11:50 +0530

    remove

--- webapp/src/main/java/io/hrushik09/ecommerce/webapp/web/controllers/LocationController.java
+++ webapp/src/main/java/io/hrushik09/ecommerce/webapp/web/controllers/LocationController.java
@@ -1,8 +1,5 @@
 package io.hrushik09.ecommerce.webapp.web.controllers;
 
-import io.hrushik09.ecommerce.webapp.clients.inventory.InventoryServiceClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 class LocationController {
-    private static final Logger log = LoggerFactory.getLogger(LocationController.class);
-    private final InventoryServiceClient inventoryServiceClient;
-
-    LocationController(InventoryServiceClient inventoryServiceClient) {
-        this.inventoryServiceClient = inventoryServiceClient;
-    }
-
     @GetMapping("/inventory/locations")
     String locationsPage(@RequestParam(name = "page", defaultValue = "1") int pageNo, Model model) {
         model.addAttribute("pageNo", pageNo);
