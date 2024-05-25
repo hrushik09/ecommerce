c2a8b98c0850413dc09fd09ba1fefccd7049de45
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-25 18:16:20 +0530

    fix tests

--- inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/domain/inventoryitems/model/CreateInventoryItemRequestBuilder.java
+++ inventory-service/src/test/java/io/hrushik09/ecommerce/inventory/domain/inventoryitems/model/CreateInventoryItemRequestBuilder.java
@@ -2,10 +2,10 @@ package io.hrushik09.ecommerce.inventory.domain.inventoryitems.model;
 
 class CreateInventoryItemRequestBuilder {
     private String productCode = "product_dummy_ak3ihajfn";
-    private Integer quantityAvailable = 1;
-    private Integer minimumStockLevel = 2;
-    private Integer maximumStockLevel = 3;
-    private Integer reorderPoint = 4;
+    private Integer quantityAvailable = 146;
+    private Integer minimumStockLevel = 89;
+    private Integer maximumStockLevel = 189;
+    private Integer reorderPoint = 105;
 
     private CreateInventoryItemRequestBuilder() {
     }
