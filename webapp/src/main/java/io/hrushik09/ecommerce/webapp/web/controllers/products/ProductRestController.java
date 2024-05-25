63fa2460fb0d008b2cafed7a952ab560b4fbf418
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-25 16:37:24 +0530

    select product code instead of typing

--- webapp/src/main/java/io/hrushik09/ecommerce/webapp/web/controllers/products/ProductRestController.java
+++ webapp/src/main/java/io/hrushik09/ecommerce/webapp/web/controllers/products/ProductRestController.java
@@ -10,6 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/inventory/products")
 class ProductRestController {
@@ -42,5 +45,22 @@ class ProductRestController {
         Product product = inventoryServiceClient.getProductByCode(code);
         log.info("response from inventory service to get product by code {}", product);
         return product;
+    }
+
+    @GetMapping("/all")
+    List<ProductSummary> getAllProducts() {
+        log.info("request to inventory service to get all products");
+        int pageNo = 1;
+        List<ProductSummary> productSummaries = new ArrayList<>();
+        while (true) {
+            PagedResult<ProductSummary> pagedResult = inventoryServiceClient.getProducts(pageNo);
+            productSummaries.addAll(pagedResult.data());
+            if (!pagedResult.hasNext()) {
+                break;
+            }
+            pageNo++;
+        }
+        log.info("response from inventory service to get all products: {}", productSummaries.size());
+        return productSummaries;
     }
 }
