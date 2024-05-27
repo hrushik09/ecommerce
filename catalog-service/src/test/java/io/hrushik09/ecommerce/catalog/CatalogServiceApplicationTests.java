0f6b73642116cc3de3050fc2b7612eff6b7b9499
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-27 23:09:38 +0530

    add ContainersConfig for integration tests

--- catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/CatalogServiceApplicationTests.java
@@ -2,8 +2,10 @@ package io.hrushik09.ecommerce.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(ContainersConfig.class)
 class CatalogServiceApplicationTests {
 
     @Test
