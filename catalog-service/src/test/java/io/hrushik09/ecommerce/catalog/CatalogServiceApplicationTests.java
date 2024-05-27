7baf75947c3eac6624c896117d28147f82208f13
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-27 23:02:36 +0530

    add catalog-service

---
+++ catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hrushik09.ecommerce.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
