d43994a6b3031ae3ecfce6fa552dfcdaa7f8c13d
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-29 01:23:17 +0530

    add end-to-end test for duplicate countries

--- catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/country/CountryEndToEndTest.java
+++ catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/country/CountryEndToEndTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CREATED;
 
 class CountryEndToEndTest extends AbstractEndToEndTest {
@@ -27,6 +28,32 @@ class CountryEndToEndTest extends AbstractEndToEndTest {
                     .body("code", hasLength(7 + 1 + 36))
                     .body("code", startsWith("country_"))
                     .body("name", equalTo("Country 2"));
+        }
+
+        @Test
+        void shouldNotCreateWhenCountryWithNameAlreadyExists() {
+            given().contentType(JSON)
+                    .body("""
+                            {
+                            "name": "Country 5"
+                            }
+                            """)
+                    .when()
+                    .post("/api/countries")
+                    .then()
+                    .statusCode(CREATED.value());
+
+            given().contentType(JSON)
+                    .body("""
+                            {
+                            "name": "Country 5"
+                            }
+                            """)
+                    .when()
+                    .post("/api/countries")
+                    .then()
+                    .statusCode(BAD_REQUEST.value())
+                    .body("detail", equalTo("Country with name Country 5 already exists"));
         }
     }
 }
