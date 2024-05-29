73319a52e8cfab058525482664cf1f6e073e1e44
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-29 09:34:09 +0530

    add jpa query

--- catalog-service/src/main/java/io/hrushik09/ecommerce/catalog/domain/country/CountryRepository.java
+++ catalog-service/src/main/java/io/hrushik09/ecommerce/catalog/domain/country/CountryRepository.java
@@ -4,9 +4,14 @@ import io.hrushik09.ecommerce.catalog.domain.country.model.CountrySummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 interface CountryRepository extends JpaRepository<CountryEntity, Long> {
     boolean existsByName(String name);
 
+    @Query("""
+            SELECT new io.hrushik09.ecommerce.catalog.domain.country.model.CountrySummary(ce.code, ce.name)
+            FROM CountryEntity ce
+            """)
     Page<CountrySummary> getCountrySummaries(Pageable pageable);
 }
