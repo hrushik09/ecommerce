cc1c17888372ce9c3dddcab7a6e963d59947837d
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-05-31 08:28:46 +0530

    add end-to-end test for get regions

--- catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/EndToEndTestDataPersister.java
+++ catalog-service/src/test/java/io/hrushik09/ecommerce/catalog/EndToEndTestDataPersister.java
@@ -3,6 +3,9 @@ package io.hrushik09.ecommerce.catalog;
 import io.hrushik09.ecommerce.catalog.domain.country.CountryService;
 import io.hrushik09.ecommerce.catalog.domain.country.model.CreateCountryCommand;
 import io.hrushik09.ecommerce.catalog.domain.country.model.CreateCountryResponse;
+import io.hrushik09.ecommerce.catalog.domain.regions.RegionService;
+import io.hrushik09.ecommerce.catalog.domain.regions.model.CreateRegionCommand;
+import io.hrushik09.ecommerce.catalog.domain.regions.model.CreateRegionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +13,15 @@ import org.springframework.context.annotation.Configuration;
 public class EndToEndTestDataPersister {
     @Autowired
     private CountryService countryService;
+    @Autowired
+    private RegionService regionService;
 
     public CreateCountryResponse country(String name) {
         CreateCountryCommand cmd = new CreateCountryCommand(name);
         return countryService.createCountry(cmd);
+    }
+
+    public CreateRegionResponse region(String countryCode, String name) {
+        return regionService.createRegion(new CreateRegionCommand(countryCode, name));
     }
 }
