64098215ef8221ff0ba4edbe3f9299e3d8774f8e
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-06-02 10:51:49 +0530

    add timeout for inventory service rest client

--- catalog-service/src/main/java/io/hrushik09/ecommerce/catalog/clients/ClientsConfig.java
+++ catalog-service/src/main/java/io/hrushik09/ecommerce/catalog/clients/ClientsConfig.java
@@ -1,14 +1,24 @@
 package io.hrushik09.ecommerce.catalog.clients;
 
 import io.hrushik09.ecommerce.catalog.ApplicationProperties;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 class ClientsConfig {
     @Bean
     RestClient restClient(RestClient.Builder builder, ApplicationProperties applicationProperties) {
-        return builder.baseUrl(applicationProperties.inventoryServiceUrl()).build();
+        return builder
+                .baseUrl(applicationProperties.inventoryServiceUrl())
+                .requestFactory(ClientHttpRequestFactories.get(ClientHttpRequestFactorySettings.DEFAULTS
+                        .withConnectTimeout(Duration.ofSeconds(5))
+                        .withReadTimeout(Duration.ofSeconds(5))
+                ))
+                .build();
     }
 }
