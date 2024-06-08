2fdb0602d6fe60792162dddd5d4dcb50ea0b9aca
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-06-08 16:55:07 +0530

    add AbstractEndToEndTest

--- user-service/src/test/java/io/hrushik09/ecommerce/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/io/hrushik09/ecommerce/userservice/UserServiceApplicationTests.java
@@ -1,12 +1,8 @@
 package io.hrushik09.ecommerce.userservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@SpringBootTest
-@Import(TestcontainersConfiguration.class)
-class UserServiceApplicationTests {
+class UserServiceApplicationTests extends AbstractEndToEndTest {
 
     @Test
     void contextLoads() {
