5ade1ff2c2acda082ba6a3eaa6977aefd57987e7
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-06-08 16:58:06 +0530

    clear database before each test with flyway

--- user-service/src/test/java/io/hrushik09/ecommerce/userservice/AbstractEndToEndTest.java
+++ user-service/src/test/java/io/hrushik09/ecommerce/userservice/AbstractEndToEndTest.java
@@ -2,12 +2,16 @@ package io.hrushik09.ecommerce.userservice;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
+@ExtendWith(ClearDatabaseExtension.class)
+@ActiveProfiles("test")
 public class AbstractEndToEndTest {
     @LocalServerPort
     protected int port;
