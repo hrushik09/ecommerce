83965c642d1eb397b96850f2308348ca86915eeb
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-06-16 22:13:42 +0530

    fix key

--- inventory-service/src/main/java/io/hrushik09/ecommerce/inventory/domain/locations/model/CreateLocationRequest.java
+++ inventory-service/src/main/java/io/hrushik09/ecommerce/inventory/domain/locations/model/CreateLocationRequest.java
@@ -7,11 +7,11 @@ import jakarta.validation.constraints.Size;
 import static io.hrushik09.ecommerce.inventory.config.DefaultApplicationProperties.SIMPLE_TEXT_REGEX;
 
 public record CreateLocationRequest(
-        @NotBlank(message = "name {not.blank}")
+        @NotBlank(message = "name {non.blank}")
         @Pattern(regexp = SIMPLE_TEXT_REGEX, message = "name {valid.characters}")
         @Size(max = 100, message = "name {max.characters}")
         String name,
-        @NotBlank(message = "address {not.blank}")
+        @NotBlank(message = "address {non.blank}")
         @Pattern(regexp = SIMPLE_TEXT_REGEX, message = "address {valid.characters}")
         @Size(max = 500, message = "address {max.characters}")
         String address
