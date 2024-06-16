f0d345495f39e2e292b6d51e2bdb3eff135e4698
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-06-16 13:57:29 +0530

    add api gateway routing

--- webapp/src/main/java/io/hrushik09/ecommerce/webapp/clients/user/UserServiceClient.java
+++ webapp/src/main/java/io/hrushik09/ecommerce/webapp/clients/user/UserServiceClient.java
@@ -2,12 +2,12 @@ package io.hrushik09.ecommerce.webapp.clients.user;
 
 import io.hrushik09.ecommerce.webapp.clients.user.customers.CreateCustomerRequest;
 import io.hrushik09.ecommerce.webapp.clients.user.customers.CreateCustomerResponse;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange("/user/api")
 public interface UserServiceClient {
-    @PostMapping("/customers")
+    @PostExchange("/customers")
     CreateCustomerResponse createCustomer(@RequestBody CreateCustomerRequest request);
 }
