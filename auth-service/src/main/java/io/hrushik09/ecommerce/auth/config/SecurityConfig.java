238207a62f5bf041f553ec2417b91537d4d4faa0
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-06-29 12:18:07 +0530

    add user details, client registration beans

--- auth-service/src/main/java/io/hrushik09/ecommerce/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/io/hrushik09/ecommerce/auth/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -12,15 +13,25 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
@@ -29,6 +40,7 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
 import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -105,22 +117,38 @@ public class SecurityConfig {
     }
 
     @Bean
-    RegisteredClientRepository registeredClientRepository() {
-        return new RegisteredClientRepository() {
-            @Override
-            public void save(RegisteredClient registeredClient) {
+    UserDetailsService userDetailsService(@Value("${temp.user.password}") String tempPassword,
+                                          @Value("${admin.user.password}") String adminPassword,
+                                          PasswordEncoder passwordEncoder) {
+        UserDetails userDetails = User.withUsername("temp")
+                .password(passwordEncoder.encode(tempPassword))
+                .authorities("read", "write")
+                .build();
+        UserDetails defaultAdmin = User.withUsername("default-admin")
+                .password(passwordEncoder.encode(adminPassword))
+                .authorities("defaultAdmin")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails, defaultAdmin);
+    }
 
-            }
-
-            @Override
-            public RegisteredClient findById(String id) {
-                return null;
-            }
-
-            @Override
-            public RegisteredClient findByClientId(String clientId) {
-                return null;
-            }
-        };
+    @Bean
+    RegisteredClientRepository registeredClientRepository(@Value("${ecommerce-default.client.secret}") String clientSecret,
+                                                          PasswordEncoder passwordEncoder) {
+        RegisteredClient registeredClient = RegisteredClient.withId("ec")
+                .clientId("ecommerce-default")
+                .clientSecret(passwordEncoder.encode(clientSecret))
+                .scope(OidcScopes.OPENID)
+                .redirectUri("http://localhost:8080/authorized")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .tokenSettings(TokenSettings.builder()
+                        .authorizationCodeTimeToLive(Duration.ofMinutes(10))
+                        .accessTokenTimeToLive(Duration.ofMinutes(10))
+                        .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                        .refreshTokenTimeToLive(Duration.ofMinutes(120))
+                        .build())
+                .build();
+        return new InMemoryRegisteredClientRepository(registeredClient);
     }
 }
